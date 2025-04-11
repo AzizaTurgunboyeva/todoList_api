cdf7005c8d039e41b9b8990cb9f40d8d653eae3e
AzizaTurgunboyeva <turgunboyevaa119@gmail.com>
2025-04-11 20:27:53 +0500

    cors problem solved

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ async function start() {
   try {
     const PORT = 3005;
     const app = await NestFactory.create(AppModule);
-      app.enableCors({
-        methods: ["GET", "POST", "PATCH", "DELETE"],
-        credentials: true,
-      });
+
+    app.enableCors({
+      origin: "http://localhost:5173", // Match your Vite frontend origin
+      methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    });
 
     await app.listen(PORT);
     console.log(`Server is running at http://localhost:${PORT}`);
